9fce52f65a89a3832bfa809a228794f52956bbec
jamesthesken <jthesken@hawaii.edu>
2023-08-25 09:04:20 -1000

    add status codes if not successful

--- pages/api/uhero/[id].ts
+++ pages/api/uhero/[id].ts
@@ -15,13 +15,21 @@ export default async function handler(
   requestHeaders.set("Authorization", `Bearer ${process.env.UHERO_KEY}`);
 
   const { id, start } = req.query;
+  try {
+    const data = await fetch(
+      `http://api.uhero.hawaii.edu/v1/package/series?u=uhero&id=${id}`,
+      { headers: requestHeaders }
+    );
 
-  const data = await fetch(
-    `http://api.uhero.hawaii.edu/v1/package/series?u=uhero&id=${id}`,
-    { headers: requestHeaders }
-  );
+    if (!data.ok) {
+      throw new Error(`Request failed with status: ${data.status}`);
+    }
 
-  const jsonData = await data.json();
-
-  res.status(200).json(jsonData);
+    const jsonData = await data.json();
+    res.status(200).json(jsonData);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred while processing the request." });
+  }
 }
